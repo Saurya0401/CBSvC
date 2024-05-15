9e20809faeb85d276ce0a5659bcbf7a2c54fdd22
Yasamin Moghaddas <ymoghadd@uci.edu>
2024-05-15 13:24:59 -0700

    updated remove_duplicate_csv

--- src/data/remove_duplicate_csv.py
+++ src/data/remove_duplicate_csv.py
@@ -1,13 +1,18 @@
 import pandas as pd
+import os
 
-# Load the CSV file
-df = pd.read_csv('input.csv')
+data_dir = ''
 
-# Columns to consider while removing duplicates
-consider_columns = ['time_seconds', 'time', 'speed', 'throttle']
+for folder in os.listdir(data_dir):
+    path = data_dir+folder
+    # Load the CSV file
+    df = pd.read_csv(path)
 
-# Remove duplicate columns except the ones specified in ignore_columns
-df_no_duplicates = df.drop_duplicates(subset=['time_seconds', 'time', 'speed', 'throttle'], keep='first', inplace=False, ignore_index=False)
+    # Columns to consider while removing duplicates
+    consider_columns = ['time_seconds', 'time', 'speed', 'throttle']
 
-# Save the data frame without duplicates (and with ignored columns) to a new CSV file
-df_no_duplicates.to_csv('output_no_duplicates.csv', index=False)
+    # Remove duplicate columns except the ones specified in ignore_columns
+    df_no_duplicates = df.drop_duplicates(subset=['time_seconds', 'time', 'speed', 'throttle'], keep='first', inplace=False, ignore_index=False)
+
+    # Save the data frame without duplicates (and with ignored columns) to a new CSV file
+    df_no_duplicates.to_csv(f'{folder[:-4]}_preprocessed.csv', index=False)
